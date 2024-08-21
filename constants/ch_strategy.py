1fbd29e2feec5f825cee2eceeaee6cd58b74efaf
imshahbaz21 <imshahbaz21@gmail.com>
2024-08-21 18:41:11 +0530

    update

--- constants/ch_strategy.py
+++ constants/ch_strategy.py
@@ -1,10 +1,7 @@
 Strategy = [
-    {"name": "SuperBull", "active": False,
-     "condition": "( {46553} ( latest adx di positive( 14 ) > latest adx di negative( 14 ) and latest close > latest open and latest close >= ( ( ( latest high - latest low ) * .8 ) + latest low ) and latest open <= ( ( ( latest high - latest low ) * .2 ) + latest low ) and latest close > latest supertrend( 10 , 3 ) and 1 day ago close < ( 1.01 * 2 days ago close ) ) )"},
-    {"name": "RSE", "active": True,
-     "condition": "( {46553} ( latest rsi( 14 ) > 55 and 1 day ago  rsi( 14 ) <= 55 and latest close > latest supertrend( 10 , 3 ) and latest ema( latest close , 3 ) > latest ema( latest close , 30 ) and latest close > latest open ) )"}
-    , {
-        "name": "Rollinger", "active": True,
-        "condition": "( {46553} ( latest low <= latest sma( latest close , 20 ) and latest close > latest open and latest close > latest supertrend( 10 , 3 ) and latest close > latest sma( latest close , 20 ) and latest high < latest upper bollinger band( 20 , 2 ) and latest rsi( 14 ) > 50 and latest close >= ( ( latest high - latest low ) * .7 ) + latest low ) )"
-    }
+  {
+    "name": "20:50",
+    "active": True,
+    "condition": "({46553} ( latest ema( latest close , 20 ) > latest ema( latest close , 50 ) and latest ema( latest close , 50 ) > latest ema( latest close , 200 ) and latest open <= latest ema( latest close , 20 ) and latest close > latest ema( latest close , 20 ) and latest rsi( 14 ) >= 55 ))"
+  }
 ]
