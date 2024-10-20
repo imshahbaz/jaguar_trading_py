4781090bc5d5222aceaa98d507a1601fea21c617
imshahbaz21 <imshahbaz21@gmail.com>
2024-10-21 00:09:04 +0530

    new strategy

--- constants/ch_strategy.py
+++ constants/ch_strategy.py
@@ -3,5 +3,10 @@ Strategy = [
     "name": "20:50",
     "active": True,
     "condition": "({46553} ( latest ema( latest close , 20 ) > latest ema( latest close , 50 ) and latest ema( latest close , 50 ) > latest ema( latest close , 200 ) and latest open <= latest ema( latest close , 20 ) and latest close > latest ema( latest close , 20 ) and latest rsi( 14 ) >= 55 ))"
-  }
+  },
+  
+  {
+    "name": "Dual Rsi",
+    "active": True,
+    "condition": "({46553} ( weekly rsi( 7 ) > 60 and latest rsi( 7 ) > 40 and 1 day ago  rsi( 7 ) <= 40 and latest close > latest open ))"} 
 ]
