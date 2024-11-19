7b8a5f31e04456e0f486f3e7a4856e642d130220
imshahbaz21 <imshahbaz21@gmail.com>
2024-11-20 04:41:51 +0530

    updating strategy

--- constants/ch_strategy.py
+++ constants/ch_strategy.py
@@ -1,12 +1,11 @@
 Strategy = [
-  {
-    "name": "20:50",
-    "active": True,
-    "condition": "({46553} ( latest ema( latest close , 20 ) > latest ema( latest close , 50 ) and latest ema( latest close , 50 ) > latest ema( latest close , 200 ) and latest open <= latest ema( latest close , 20 ) and latest close > latest ema( latest close , 20 ) and latest rsi( 14 ) >= 55 ))"
-  },
-  
-  {
-    "name": "Dual Rsi",
-    "active": True,
-    "condition": "({46553} ( weekly rsi( 7 ) > 60 and latest rsi( 7 ) > 40 and 1 day ago  rsi( 7 ) <= 40 and latest close > latest open ))"} 
+    {
+        "name": "20:50",
+        "active": True,
+        "condition": "({46553} ( latest ema( latest close , 20 ) > latest ema( latest close , 50 ) and latest ema( latest close , 50 ) > latest ema( latest close , 200 ) and latest open <= latest ema( latest close , 20 ) and latest close > latest ema( latest close , 20 ) and latest rsi( 14 ) >= 55 ))"
+    },
+    {
+        "name": "Super Rsi",
+        "active": True,
+        "condition": "( {46553} ( latest close > latest open and latest close > latest supertrend( 10 , 3 ) and latest rsi( 14 ) > 60 and 1 day ago  rsi( 14 ) <= 60 ) )"}
 ]
