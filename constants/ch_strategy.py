de0cdd9bb67f3be3892a0c86ab4c03bfd1aea5d3
imshahbaz21 <imshahbaz21@gmail.com>
2025-02-27 04:16:04 +0530

    Vollinger

--- constants/ch_strategy.py
+++ constants/ch_strategy.py
@@ -1,11 +1,17 @@
 Strategy = [
     {
         "name": "20:50",
-        "active": True,
+        "active": False,
         "condition": "({46553} ( latest ema( latest close , 20 ) > latest ema( latest close , 50 ) and latest ema( latest close , 50 ) > latest ema( latest close , 200 ) and latest open <= latest ema( latest close , 20 ) and latest close > latest ema( latest close , 20 ) and latest rsi( 14 ) >= 55 ))"
     },
     {
         "name": "Super Rsi",
         "active": True,
-        "condition": "( {46553} ( latest close > latest open and latest close > latest supertrend( 10 , 3 ) and latest rsi( 14 ) > 60 and 1 day ago  rsi( 14 ) <= 60 ) )"}
+        "condition": "( {46553} ( latest close > latest open and latest close > latest supertrend( 10 , 3 ) and latest rsi( 14 ) > 60 and 1 day ago  rsi( 14 ) <= 60 ) )"
+    }
+    , {
+        "name": "Vollinger",
+        "active": True,
+        "condition": "( {46553} ( latest close > latest open and latest close > latest sma( latest close , 20 ) and latest volume > 1 day ago volume and latest low <= latest sma( latest close , 20 ) and latest close >= ( ( latest high - latest low ) * 0.7 ) + latest low ) )"
+    }
 ]
